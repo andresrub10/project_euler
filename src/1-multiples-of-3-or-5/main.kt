4c42710ecbc1a518f6801031b5a8bf74525cde3c
Andres Rubio <a.rubio@horseanalytics.de>
2022-02-10 14:46:49 -0400

    first commit

---
+++ src/1-multiples-of-3-or-5/main.kt
@@ -0,0 +1,10 @@
+fun code() {
+    var n = 2
+    var n2 = 4
+    println(n + n2)
+}
+
+fun main() {
+    println("Hello world!")
+    code()
+}
---
+++ src/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello world!")
+    code()
+}
+
+fun code(){
+    var n = 2
+    var n2 = 4
+    println(n+n2)
+}
